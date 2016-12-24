f6791c2ff2ca015de74b7e9975ad2ec2f33813f5
PadamSethia <padamsethia5@gmail.com>
2016-12-24 21:01:40 +0530

    Shakespeare Dataset added

---
+++ index.py
@@ -0,0 +1,10 @@
+vocab_size = 8000;
+unknown_token = "UNKNOWN_TOKEN";
+start_token = "SENTENCE_START";
+end_token = "SENTENCE_END";			
+
+print "Reading William Fucktards novels" # Read the CSV File will_play_text.CSV;
+
+with open("will_play_text.csv") as f:
+	reader = ;
+	
