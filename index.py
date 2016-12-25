cf165c023a8121f8e8cbbca470ff8f87e1f1ba99
PadamSethia <padamsethia5@gmail.com>
2016-12-25 17:11:04 +0530

    Data Cleaned

--- index.py
+++ index.py
@@ -1,10 +1,19 @@
-vocab_size = 8000;
-unknown_token = "UNKNOWN_TOKEN";
-start_token = "SENTENCE_START";
-end_token = "SENTENCE_END";			
+import csv
+import nltk
+import itertools
 
-print "Reading William Fucktards novels" # Read the CSV File will_play_text.CSV;
+vocab_size = 8000
+unknown_token = "UNKNOWN_TOKEN"
+start_token = "SENTENCE_START"
+end_token = "SENTENCE_END"
+# Read the CSV File will_play_text.CSV;
+print "Reading William Fucktards novels....  \n"
 
-with open("will_play_text.csv") as f:
-	reader = ;
-	
+with open("will_play_text.csv", "rb") as f:
+    reader = csv.reader(f)
+    sentence = list(reader)
+    for i in range(800):
+        sentence[:800][i][5]
+    #for i in xrange(900):
+     #   sen = sentence[0:100][i][5]
+ 
